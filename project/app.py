287b070aae2b31b4fba36b283653fe9d919c7e33
admratuiste <amratuiste@up.edu.ph>
2022-05-31 20:49:02 +0800

    Modified input validation

--- project/app.py
+++ project/app.py
@@ -1,122 +1,215 @@
+from datetime import datetime
+from dateutil import parser
 import mysql.connector as mariadb
 
 # establish connection to mariadb database
 db = mariadb.connect(
     host='localhost', 
     user='user1', 
-    password='12345678', 
+    password='12345678',
     database="task_db"
 )
 
-cursor = db.cursor()
+cursor = db.cursor(buffered=True)
 
 # create database and tables
-def addTask(taskTitle, dateCreated, deadline, taskDescription):
+def validateDateFormat(date):
     try:
-        insertQuery = """INSERT INTO task (task_title, date_created, deadline, task_description) VALUES (%s, %s, %s, %s)"""
-        data = (taskTitle, dateCreated, deadline, taskDescription)
-        cursor.execute(insertQuery, data)
+        datetime.strptime(date, "%Y-%m-%d")
+        return True
+    except ValueError:
+        print("Invalid date. Date format should be YYYY-MM-DD.")
+        return False
+    
+def validateDate(date1, date2):
+    try:
+        date1 = parser.parse(date1)
+        date2 = parser.parse(date2)
+        dateDiff = date2 - date1
+        if dateDiff.days >= 0:
+            return True
+    except:
+        return False
+        
+    
+def checkDatabase():
+    cursor.execute("SELECT * FROM task")
+    record = cursor.fetchone()
+    if not record:
+        print("\nThere are no tasks.")
+        return False
+    else:
+        return True
+
+def addTask(db, cursor):
+    try:
+        taskTitle = input("\nEnter task title: ")
+        while True:
+            dateCreated = input("Enter date created (YYYY-MM-DD): ")
+            if validateDateFormat(dateCreated) == True:
+                break
+        while True:
+            deadline = input("Enter deadline (YYYY-MM-DD): ")
+            if validateDateFormat(deadline) == True:
+                if validateDate(dateCreated, deadline) == True:
+                    break
+                else:
+                    print("Invalid deadline. Deadline must not be before the date created.")
+        taskDescription = input("Enter task description: ")
+        cursor.execute("INSERT INTO task (task_title, date_created, deadline, task_description) VALUES (%s, %s, %s, %s)", (taskTitle, dateCreated, deadline, taskDescription))
         db.commit()
-        print("Added task successfully.")
+        print("\nAdded task successfully.")
         
     except mariadb.Error as e:
-        print("Failed to add task. Error: {}".format(e))
+        print(f"Failed to add task. Error: {e}")
         
-def editTaskTitle(taskTitle, taskNo):
+def editTaskTitle(db, cursor):
     try:
+        if checkDatabase() == False:
+            return
+        taskNo = input("\nEnter task number: ")
         # check if taskNo exists first in database, if not return from function
         cursor.execute("SELECT * FROM task WHERE taskno = %s", (taskNo,))
-        if cursor.rowcount == 0:
-            print("The task number does not exist in the task table.")
+        record = cursor.fetchone()
+        if not record:
+            print("\nThere are no tasks that match.")
             return
-        updateQuery = """UPDATE task SET task_title = %s WHERE taskno = %s"""
-        data = (taskTitle, taskNo)
-        cursor.execute(updateQuery, data)
+        taskTitle = input("Enter new task title: ")
+        cursor.execute("UPDATE task SET task_title = %s WHERE taskno = %s", (taskTitle, taskNo))
         db.commit()
-        print("Edited task title successfully.")
+        print("\nEdited task title successfully.")
         
     except mariadb.Error as e:
-        print("Failed to edit task title. Error: {}".format(e))
+        print(f"Failed to edit task title. Error: {e}")
         
-def editDateCreated(dateCreated, taskNo):
+def editDateCreated(db, cursor):
     try:
+        if checkDatabase() == False:
+            return
+        taskNo = input("\nEnter task number: ")
         cursor.execute("SELECT * FROM task WHERE taskno = %s", (taskNo,))
-        if cursor.rowcount == 0:
-            print("The task number does not exist in the task table.")
+        record = cursor.fetchone()
+        deadline = str(record[3])
+        dateCompleted = str(record[4])
+        if not record:
+            print("\nThere are no tasks that match.")
             return
-        updateQuery = """UPDATE task SET date_created = %s WHERE taskno = %s"""
-        data = (dateCreated, taskNo)
-        cursor.execute(updateQuery, data)
+        while True:
+            dateCreated = input("Enter new date created (YYYY-MM-DD): ")
+            if validateDateFormat(dateCreated) == True:
+                if validateDate(dateCreated, deadline) == True:
+                    if dateCompleted != "None":
+                        if validateDate(dateCreated, dateCompleted) == True:
+                            break
+                        else:
+                            print("Invalid date created. Date created must not be after the date completed.")
+                    else:
+                        break
+                else:
+                    print("Invalid date created. Date created must not be after the deadline.")
+        cursor.execute("UPDATE task SET date_created = %s WHERE taskno = %s", (dateCreated, taskNo))
         db.commit()
-        print("Edited date created successfully.")
+        print("\nEdited date created successfully.")
         
     except mariadb.Error as e:
-        print("Failed to edit date created. Error: {}".format(e))
+        print(f"Failed to edit date created. Error: {e}")
         
-def editDeadline(deadline, taskNo):
+def editDeadline(db, cursor):
     try:
+        if checkDatabase() == False:
+            return
+        taskNo = input("\nEnter task number: ")
         cursor.execute("SELECT * FROM task WHERE taskno = %s", (taskNo,))
-        if cursor.rowcount == 0:
-            print("The task number does not exist in the task table.")
+        record = cursor.fetchone()
+        dateCreated = str(record[2])
+        if not record:
+            print("\nThere are no tasks that match.")
             return
-        updateQuery = """UPDATE task SET deadline = %s WHERE taskno = %s"""
-        data = (deadline, taskNo)
-        cursor.execute(updateQuery, data)
+        while True:
+            deadline = input("Enter new deadline (YYYY-MM-DD): ")
+            if validateDateFormat(deadline) == True:
+                if validateDate(dateCreated, deadline) == True:
+                    break
+                else:
+                    print("Invalid deadline. Deadline must not be before the date created.")
+        cursor.execute("UPDATE task SET deadline = %s WHERE taskno = %s", (deadline, taskNo))
         db.commit()
-        print("Edited deadline successfully.")
+        print("\nEdited deadline successfully.")
         
     except mariadb.Error as e:
-        print("Failed to edit deadline. Error: {}".format(e))
+        print(F"Failed to edit deadline. Error: {e}")
         
-def editDateCompleted(dateCompleted, taskNo):
+def editDateCompleted(db, cursor):
     try:
-        cursor.execute("SELECT * FROM task WHERE taskno = %s")
-        if cursor.rowcount == 0:
-            print("The task number does not exist in the task table")
+        if checkDatabase() == False:
             return
-        updateQuery = """UPDATE task SET date_completed = %s WHERE taskno = %s"""
-        data = (dateCompleted, taskNo)
-        cursor.execute(updateQuery, data)
-        db.commit()
-        print("Edited date completed successfully.")
-        
-    except mariadb.Error as e:
-        print("Failed to edit date completed. Error: {}".format(e))
-        
-def editTaskDescription(taskDescription, taskNo):
-    try:
+        taskNo = input("\nEnter task number: ")
         cursor.execute("SELECT * FROM task WHERE taskno = %s", (taskNo,))
-        if cursor.rowcount == 0:
-            print("The task number does not exist in the task table.")
+        record = cursor.fetchone()
+        dateCreated = str(record[2])
+        if not record:
+            print("\nThere are no tasks that match.")
             return
-        updateQuery = """UPDATE task SET task_description = %s WHERE taskno = %s"""
-        data = (taskDescription, taskNo)
-        cursor.execute(updateQuery, data)
+        while True:
+            dateCompleted = input("Enter new date completed (YYYY-MM-DD): ")
+            if validateDateFormat(dateCompleted) == True:
+                if validateDate(dateCreated, dateCompleted) == True:
+                    break
+                else:
+                    print("Invalid date completed. Date completed must not be before the date created.")
+        cursor.execute("UPDATE task SET date_completed = %s WHERE taskno = %s", (dateCompleted, taskNo))
         db.commit()
-        print("Edited task description successfully.")
+        print("\nEdited date completed successfully.")
         
     except mariadb.Error as e:
-        print("Failed to edit task description. Error: {}".format(e))
+        print(f"Failed to edit date completed. Error: {e}")
         
-def deleteTask(taskNo):
+def editTaskDescription(db, cursor):
     try:
-        cursor.execute("SELECT * FROM task WHERE taskno = %s", (taskNo,))
-        if cursor.rowcount == 0:
-            print("The task number does not exist in the task table.")
+        if checkDatabase() == False:
             return
-        deleteQuery = """DELETE FROM task WHERE taskno = %s"""
-        cursor.execute(deleteQuery, (taskNo,))
+        taskNo = input("\nEnter task number: ")
+        cursor.execute("SELECT * FROM task WHERE taskno = %s", (taskNo,))
+        record = cursor.fetchone()
+        if not record:
+            print("\nThere are no tasks that match.")
+            return
+        taskDescription = input("Enter new task description: ")
+        cursor.execute("UPDATE task SET task_description = %s WHERE taskno = %s", (taskDescription, taskNo))
         db.commit()
-        print("Deleted task successfully.")
+        print("\nEdited task description successfully.")
         
     except mariadb.Error as e:
-        print("Failed to delete task. Error: {}".format(e))
+        print(f"Failed to edit task description. Error: {e}")
+        
+def deleteTask(db, cursor):
+    try:
+        if checkDatabase() == False:
+            return
+        taskNo = input("\nEnter task number: ")
+        cursor.execute("SELECT * FROM task WHERE taskno = %s", (taskNo,))
+        record = cursor.fetchone()
+        if not record:
+            print("\nThere are no tasks that match.")
+            return
+        cursor.execute("DELETE FROM task WHERE taskno = %s", (taskNo,))
+        # sorts the taskno after deleting a task
+        cursor.execute("SET @count = 0")
+        cursor.execute("UPDATE task SET taskno = @count:= @count + 1")
+        cursor.execute("ALTER TABLE task AUTO_INCREMENT = 1")
+        db.commit()
+        print("\nDeleted task successfully.")
+        
+    except mariadb.Error as e:
+        print(f"Failed to delete task. Error: {e}")
         
 def viewAllTask():
     try:
-        selectQuery = """SELECT * FROM task"""
-        cursor.execute(selectQuery)
+        cursor.execute("SELECT * FROM task")
         data = cursor.fetchall()
+        if not data:
+            print("\nThere are no tasks left to do. Congratulations!")
+            return
         print("\nTotal number of tasks: ", cursor.rowcount)
         print("\nTo-Do List")
         for row in data:
@@ -125,57 +218,41 @@ def viewAllTask():
             print("Date Created: ", row[2])
             print("Deadline: ", row[3])
             print("Date Completed: ", row[4])
-            print("Task Status: ", row[5])
+            print("Task Description: ", row[5])
+            print("Task Status (0 - Ongoing, 1 - Finished): ", row[6])
     
     except mariadb.Error as e:
-        print("Failed to view all tasks. Error: {}".format(e))
+        print(f"Failed to view all tasks. Error: {e}")
 
 def editOptions(option):
-    while option != 0:
-        match option:
-            case '1':
-                data1 = input("Enter new task title: ")
-                data2 = input("Enter task number: ")
-                editTaskTitle(data1, data2)
-            case '2':
-                data1 = input("Enter new date (YYYY-MM-DD): ")
-                data2 = input("Enter task number: ")
-                editDateCreated(data1, data2)
-            case '3':
-                data1 = input("Enter new deadline (YYYY-MM-DD): ")
-                data2 = input("Enter task number: ")
-                editDeadline(data1, data2)
-            case '4':
-                data1 = input("Enter new date (YYYY-MM-DD): ")
-                data2 = input("Enter task number: ")
-                editDateCompleted(data1, data2)
-            case '5':
-                data1 = input("Enter new task description: ")
-                data2 = input("Enter task number: ")
-                editTaskDescription(data1, data2)
-            case '0':
-                main()
-            case _:
-                print("Invalid input.")
-        option = input("\nEdit option: ")
+    match option:
+        case '1':
+            editTaskTitle(db, cursor)
+        case '2':
+            editDateCreated(db, cursor)
+        case '3':
+            editDeadline(db, cursor)
+        case '4':
+            editDateCompleted(db, cursor)
+        case '5':
+            editTaskDescription(db, cursor)
+        case '0':
+            main()
+        case _:
+            print("\nInvalid input.")
 
 # app loop which matches choice to its corresponding action
 def app_loop(choice):
     while choice != 0:
         match choice:
             case '1':
-                data1 = input("Enter task title: ")
-                data2 = input("Enter date created (YYYY-MM-DD): ")
-                data3 = input("Enter deadline (YYYY-MM-DD): ")
-                data4 = input("Enter task description: ")
-                addTask(data1, data2, data3, data4)
+                addTask(db, cursor)
             case '2':
                 print('\n------EDIT TASK------', '[1] Edit Task Title', '[2] Edit Date Created', '[3] Edit Deadline', '[4] Edit Date Completed', '[5] Edit Task Description', '[0] Back', sep='\n')
                 option = input("\nEdit option: ")
                 editOptions(option)
             case '3':
-                taskNo = input("Enter task number: ")
-                deleteTask(taskNo)
+                deleteTask(db, cursor)
             case '4':
                 viewAllTask()
             case '5':
@@ -193,10 +270,12 @@ def app_loop(choice):
             case '11':
                 print("Viewing task!")
             case '0':
-                print("Exiting app!")
+                cursor.close()
+                db.close()
+                print("\nExiting app!")
                 quit()
             case _:
-                print("Invalid input.")
+                print("\nInvalid input.")
         print('\n------TO-DO APP------', '[1] Add/Create Task', '[2] Edit Task', '[3] Delete Task', '[4] View All Tasks', '[5] Mark Task as done', '[6] Add Category', '[7] Edit Category', '[8] Delete Category', '[9] View Category', '[10] Add a Task to a Category', '[11] View Task (per day, month)', '[0] Exit', sep='\n')
         choice = input("\nChoice: ")
         
